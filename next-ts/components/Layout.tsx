5be3a5d4b029d38adfcf2f056321fe638b47de30
gustavthesteel85 <gustavthesteel85@gmail.com>
2021-09-12 17:29:37 +0900

    jest

---
+++ next-ts/components/Layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
+import style from "../static/Style";
+
+const Layout = (props) => {
+  return (
+    <>
+      {/* <Header header={props.header} title={props.title}/>
+                {props.children}
+            <Footer footer={'copyright toshi'} /> */}
+      <Head>
+        <title>{props.title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {style}
+      <Header header={props.header} title={props.title} />
+      {props.children}
+      <Footer footer="copyright toshi" />
+    </>
+  );
+};
+
+export default Layout;
